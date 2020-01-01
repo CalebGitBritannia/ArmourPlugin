219834de478f05b94545b8d7d9da7b173a592e84
CalebGitBritannia <calebbritannia@gmail.com>
2020-01-01 13:16:31 +0000

    Initial plugin information

---
+++ src/main/java/com/caleb/armourplugin/ArmourPlugin.java
@@ -0,0 +1,10 @@
+package com.caleb.armourplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArmourPlugin extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        System.out.println("Plugin works!");
+    }
+}
