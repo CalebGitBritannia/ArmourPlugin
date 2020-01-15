89352b31b5dff88e2d2b16dd3ae6e1447e7abd95
CalebGitBritannia <calebbritannia@gmail.com>
2020-01-15 16:43:46 +0000

    Made ice move that restores afterwards

--- src/main/java/com/caleb/armourplugin/listeners/EventsClass.java
+++ src/main/java/com/caleb/armourplugin/listeners/EventsClass.java
@@ -1,9 +1,11 @@
 package com.caleb.armourplugin.listeners;
 
 import com.caleb.armourplugin.CalebAPI;
+import com.caleb.armourplugin.RollBack.RollBack;
 import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Particle;
 import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -16,6 +18,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class EventsClass implements Listener {
 
@@ -36,6 +39,25 @@ public class EventsClass implements Listener {
                         player.getWorld().playSound(player.getLocation(), Sound.ANVIL_BREAK, 10, 1);
                         player.getWorld().playEffect(player.getLocation(), Effect.COLOURED_DUST, 1);
                         living.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * 10, 3));
+                        RollBack newRoll = new RollBack(player.getLocation(), 6);
+                        for (int x = -6; x < 6; x++ ) {
+                            for (int z = -6; z < 6; z++) {
+                                Location location = player.getLocation();
+                                location.setZ(player.getLocation().getZ() + z);
+                                location.setX(player.getLocation().getX() + x);
+                                for(int y = 255; y > 0; y--) {
+                                    location.setY(y);
+                                    if (location.getBlock().getType() != Material.AIR) {
+                                        int guesser = (int) (Math.random() * 10);
+                                        if (guesser > 8) {
+                                            newRoll.addBlock(location.getBlock());
+                                            location.getBlock().setType(Material.PACKED_ICE);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        } BukkitTask task1 = new EventsClass.throwTask(newRoll).runTaskLater(plugin, 7*10);
 
 
                     }
@@ -44,6 +66,19 @@ public class EventsClass implements Listener {
                 System.out.println("He is not wearing");
                 e.setCancelled(true);
             }
+        }
+    }
+
+    public static class throwTask extends BukkitRunnable {
+
+        private RollBack rollBack;
+
+        throwTask(RollBack rollBack) {
+            this.rollBack = rollBack;
+        }
+
+        public void run() {
+            rollBack.restoreState();
         }
     }
 }
